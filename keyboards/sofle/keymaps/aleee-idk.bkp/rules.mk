bd23e91e4f9d307c0c7002cb4defc3a785acd1e9
Alexander Navarro <ale.navarro.parra@gmail.com>
2022-05-24 21:25:47 -0400

    update personal config

---
+++ keyboards/sofle/keymaps/aleee-idk.bkp/rules.mk
@@ -0,0 +1,26 @@
+OLED_ENABLE = yes
+ENCODER_ENABLE = yes
+EXTRAKEY_ENABLE = yes
+MOUSEKEY_ENABLE = yes
+WPM_ENABLE = no
+# RGBLIGHT_ENABLE = yes
+
+#Debug options
+VERBOSE = no
+CONSOLE_ENABLE = no
+COMMAND_ENABLE = no
+
+LTO_ENABLE = yes
+
+SPLIT_KEYBOARD = yes
+
+VIA_ENABLE = yes
+
+POINTING_DEVICE_ENABLE = yes
+POINTING_DEVICE_DRIVER = pimoroni_trackball
+
+ifeq ($(PROMICRO), yes)
+  BOOTLOADER = caterina
+else ifeq ($(ELITEC), yes)
+  BOOTLOADER = atmel-dfu
+endif
